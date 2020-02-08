3cd4c83cf4b3fd2ea0ff9d4fb56e4ed6ec3d6f07
Gbenga <gbengulo005@gmail.com>
2020-02-08 15:34:14 +0100

    I upgraded the python pip version

---
+++ miniBlog/myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ADERONKE\Desktop\MiniBloggerApp\miniBlog\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
