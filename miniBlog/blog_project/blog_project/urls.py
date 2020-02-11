0813572175ab666716205f9915b45d502831bf60
Gbenga <gbengulo005@gmail.com>
2020-02-11 14:02:54 +0100

    I created a url for my accounts views

--- miniBlog/blog_project/blog_project/urls.py
+++ miniBlog/blog_project/blog_project/urls.py
@@ -20,4 +20,6 @@ from . import views
 urlpatterns = [
     path('', views.HomePage.as_view(), name="home"),
     path('admin/', admin.site.urls),
+    path('accounts/', include("accounts.urls",namespace="accounts")),
+    path('accounts/', include("django.contrib.auth.urls")),
 ]
